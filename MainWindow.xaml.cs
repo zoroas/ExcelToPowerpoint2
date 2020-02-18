e05e26207577bb3a9277cddec09df343e7ca0c38
Marc0Costa <marco.bc.costa@caislisbon.org>
2020-02-18 16:57:57 +0000

    Initial edit of methods

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,248 @@ namespace ExcelToPowerpoint2
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            this.ExcelFileName = MySettings.Default.SettingExcelFileName;
+            this.PowerPointTemplateFileName = MySettings.Default.SettingPowerPointTemplateFileName;
+            this.PowerPointOutputFileName = MySettings.Default.SettingPowerPointOutputFileName;
+            this.PhotoFolderName = MySettings.Default.SettingPhotoFolderName;
+            this.NumberOfColumns = MySettings.Default.SettingNumberOfColumns;
+            this.RowNumberToGetColumns = MySettings.Default.SettingRowNumberToGetColumns;
+        }
+
+        public static readonly DependencyProperty ExcelFileNameProperty =
+            DependencyProperty.Register("ExcelFileName",
+                typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+        
+        public static readonly DependencyProperty PowerPointTemplateFileNameProperty =
+            DependencyProperty.Register("PowerPointTemplateFileName",
+                typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty NumberOfColumnsProperty =
+            DependencyProperty.Register("NumberOfColumns",
+                typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty RowNumberToGetColumnsProperty =
+            DependencyProperty.Register("RowNumberToGetColumns",
+                 typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty PowerPointOutputFileNameProperty =
+            DependencyProperty.Register("PowerPointOutputFileName",
+                typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty PhotoFolderNameProperty =
+            DependencyProperty.Register("PhotoFolderName",
+                typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        public string ExcelFileName
+        {
+            get { return (string)GetValue(ExcelFileNameProperty); }
+            set
+            {
+                SetValue(ExcelFileNameProperty, value);
+                MySettings.Default.SettingExcelFileName = value;
+            }
+        }
+
+        public string PowerPointTemplateFileName
+        {
+            get { return (string)GetValue(PowerPointTemplateFileNameProperty); }
+            set
+            {
+                SetValue(PowerPointTemplateFileNameProperty, value);
+                MySettings.Default.SettingPowerPointTemplateFileName = value;
+            }
+        }
+
+        public string PowerPointOutputFileName
+        {
+            get { return (string)GetValue(PowerPointOutputFileNameProperty); }
+            set
+            {
+                SetValue(PowerPointOutputFileNameProperty, value);
+                MySettings.Default.SettingPowerPointOutputFileName = value;
+            }
+        }
+
+        public string RowNumberToGetColumns
+        {
+            get { return (string)GetValue(RowNumberToGetColumnsProperty); }
+            set
+            {
+                SetValue(RowNumberToGetColumnsProperty, value);
+            }
+        }
+
+        public string NumberOfColumns
+        {
+            get { return (string)GetValue(NumberOfColumnsProperty); }
+            set
+            {
+                SetValue(NumberOfColumnsProperty, value);
+            }
+        }
+
+        public string PhotoFolderName
+        {
+            get { return (string)GetValue(PhotoFolderNameProperty); }
+            set
+            {
+                SetValue(PhotoFolderNameProperty, value);
+            }
+        }
+
+        private void TBExcelDataFile_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void TBPowerPoint_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void TBPhotoFolder_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void TBPhotoFolder_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    this.PhotoFolderName = files[0];
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void ButtonPhotoFolder_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            System.Windows.Forms.FolderBrowserDialog dlg = new System.Windows.Forms.FolderBrowserDialog();
+
+            dlg.ShowDialog();
+            // Get the selected file name and display in a TextBox 
+            if (dlg.SelectedPath != null)
+            {
+                // Open document 
+                string folder = dlg.SelectedPath;
+                this.PhotoFolderName = folder;
+            }
+        }
+
+        private void TBExcelDataFile_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                this.ExcelFileName = files[0];
+            }
+        }
+
+        private void TBPowerPoint_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                this.PowerPointTemplateFileName = files[0];
+            }
+        }
+
+
+        private void TBOutputPowerPoint_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    this.PowerPointOutputFileName = files[0];
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void TBOutputPowerPoint_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void ButtonExcelDataFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".xlsx";
+            dlg.Filter = "Excel documents (.xlsx)|*.xlsx|Excel old documents (.xls)|*.xls|All files (*.*)|*.*";
+
+            // Get the selected file name and display in a TextBox 
+            if (dlg.ShowDialog() == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                this.ExcelFileName = filename;
+            }
+        }
+
+        private void ButtonPowerPointTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".pptx";
+            dlg.Filter = "PowerPoint documents (.pptx)|*.pptx|PowerPoint old documents (.ppt)|*.ppt|All files (*.*)|*.*";
+
+            // Get the selected file name and display in a TextBox 
+            if (dlg.ShowDialog() == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                this.PowerPointTemplateFileName = filename;
+            }
+        }
+
+        private void ButtonOutputPowerPoint_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".pptx";
+            dlg.Filter = "PowerPoint documents (.pptx)|*.pptx|PowerPoint old documents (.ppt)|*.ppt|All files (*.*)|*.*";
+
+            // Get the selected file name and display in a TextBox 
+            if (dlg.ShowDialog() == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                this.PowerPointOutputFileName = filename;
+            }
+        }
+
+        private void ButtonOptions_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonCreateFile_Click(object sender, RoutedEventArgs e)
+        {
+            Cursor = Cursors.Wait;
+
+            PowerPointCreator c = new PowerPointCreator(this.ExcelFileName, this.PowerPointTemplateFileName,
+                this.PowerPointOutputFileName, this.RowNumberToGetColumns,
+                this.NumberOfColumns, this.PhotoFolderName);
+            if (c.HasError())
+                MessageBox.Show(c.GetError());
+            Cursor = Cursors.Arrow;
         }
     }
 }
---
+++ Model/PowerPointCreator.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+using DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace ExcelToPowerpoint2
+{
+    public class PowerPointCreator
+    {
+        private bool hasError = true;
+        private string error = "";
+
+        public bool HasError()
+        {
+            return this.hasError;
+        }
+
+        public string GetError()
+        {
+            return this.error;
+        }
+
+        //public static void InsertImageInSlide(PowerPoint.Slide slide, PowerPoint.Shape shape, string imagePath)
+        //{
+        //    float left = shape.Left;
+        //    float top = shape.Top;
+        //    float width = shape.Width;
+        //    float height = shape.Height;
+        //    slide.Shapes.AddPicture(imagePath, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoTrue, left, top, width, height);
+        //}
+
+        public PowerPointCreator(
+                String excelFile, string pptTemplate, string pptOutput,
+                string rowNumberToGetColumnsStr, string numberOfColumnsStr, String photoFolder)
+        {
+
+            using (PresentationDocument presentationDocument = PresentationDocument.Open(pptTemplate, false))
+            {
+                // Verify that the presentation document exists.
+                if (presentationDocument == null)
+                {
+                    throw new ArgumentNullException("presentationDocument");
+                }
+
+                // Get the presentation part of the presentation document.
+                PresentationPart presentationPart = presentationDocument.PresentationPart;
+
+                // Verify that the presentation part and presentation exist.
+                if (presentationPart == null || presentationPart.Presentation == null)
+                    return;
+
+                // Get the Presentation object from the presentation part.
+                Presentation presentation = presentationPart.Presentation;
+
+                // Verify that the slide ID list exists.
+                if (presentation.SlideIdList == null)
+                    return;
+
+                // Get the collection of slide IDs from the slide ID list.
+                var slideIds = presentation.SlideIdList.ChildElements;
+
+                // If the slide ID is in range...
+                if (slideIds.Count != 1)
+                    return;
+
+                // Get the relationship ID of the slide.
+                string slidePartRelationshipId = (slideIds[0] as SlideId).RelationshipId;
+
+                // Get the specified slide part from the relationship ID.
+                SlidePart slidePart = (SlidePart)presentationPart.GetPartById(slidePartRelationshipId);
+
+                // Pass the slide part to the next method, and
+                // then return the array of strings that method
+                // returns to the previous method.
+
+                LinkedList<string> texts = new LinkedList<string>();
+                // Iterate through all the paragraphs in the slide.
+                foreach (var paragraph in slidePart.Slide.Descendants<DocumentFormat.OpenXml.Drawing.Paragraph>())
+                {
+                    // Create a new string builder.                    
+                    StringBuilder paragraphText = new StringBuilder();
+
+                    // Iterate through the lines of the paragraph.
+                    foreach (var text in paragraph.Descendants<DocumentFormat.OpenXml.Drawing.Text>())
+                    {
+                        // Append each line to the previous lines.
+                        paragraphText.Append(text.Text);
+                    }
+
+                    if (paragraphText.Length > 0)
+                    {
+                        // Add each paragraph to the linked list.
+                        texts.AddLast(paragraphText.ToString());
+                    }
+                }
+
+            }
+        }
+
+
+
+        //    try
+        //    {
+        //        int rowNumberToGetColumns = int.Parse(rowNumberToGetColumnsStr);
+        //        int numberOfColumns = int.Parse(numberOfColumnsStr);
+
+        //        // Create the PowerPoint
+        //        oPowerPoint = new PowerPoint.Application();
+        //        oPres = oPowerPoint.Presentations;
+        //        File.Copy(pptTemplate, pptOutput);
+
+        //        oPre = oPres.Open(pptOutput);
+        //        oSlides = oPre.Slides;
+
+        //        // Open Excel
+        //        oExcel = new Excel.Application();
+        //        oWorkBook = oExcel.Workbooks.Open(excelFile);
+        //        oSheet = oWorkBook.Sheets[1];
+
+        //        // Iterate over lines
+        //        bool doMore = true;
+        //        int row = rowNumberToGetColumns;
+
+        //        while (doMore)
+        //        {
+
+        //            row++;
+
+        //            oSlide = oSlides[1];
+        //            oSlide.Duplicate();
+        //            oSlide = oSlides[2];
+        //            doMore = false;
+        //            for (int col = 1; col <= numberOfColumns; col++)
+        //            {
+        //                Excel.Range label = oSheet.Cells[rowNumberToGetColumns, col];
+        //                String myLabel = (":" + label.Value + ":").ToLower();
+        //                Excel.Range cell = oSheet.Cells[row, col];
+
+        //                String myValue = "";
+
+        //                try
+        //                {
+        //                    if (cell.Value != null)
+        //                    {
+        //                        DateTime date = cell.Value;
+        //                        myValue = date.ToString(this.GetDateFormat());
+        //                    }
+        //                }
+        //                catch (Exception)
+        //                {
+        //                    try
+        //                    {
+        //                        myValue = cell.Value.ToString();
+        //                    }
+        //                    catch (Exception)
+        //                    {
+        //                    }
+        //                }
+
+        //                if (myValue != "")
+        //                {
+        //                    doMore = true;
+        //                }
+
+        //                if (myLabel == ":photo:")
+        //                {
+        //                    myValue = photoFolder + "/" + myValue + ".jpg";
+        //                    for (int i = 1; i <= oSlide.Shapes.Count; i++)
+        //                    {
+        //                        PowerPoint.Shape shape = oSlide.Shapes[i];
+        //                        if (shape.HasTextFrame == Microsoft.Office.Core.MsoTriState.msoTrue)
+        //                        {
+        //                            String shapeLabel = shape.TextFrame.TextRange.Text;
+        //                            if (shapeLabel.ToLower() == ":photo:" && File.Exists(myValue))
+        //                            {
+        //                                float left = shape.Left;
+        //                                float top = shape.Top;
+        //                                float width = shape.Width;
+        //                                float height = shape.Height;
+        //                                oSlide.Shapes.AddPicture(myValue, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoTrue, left, top, width, height);
+        //                                shape.Delete();
+        //                            }
+        //                        }
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    for (int i = 1; i <= oSlide.Shapes.Count; i++)
+        //                    {
+        //                        PowerPoint.Shape shape = oSlide.Shapes[i];
+        //                        if (shape.HasTextFrame == Microsoft.Office.Core.MsoTriState.msoTrue)
+        //                        {
+        //                            String oldValue = shape.TextFrame.TextRange.Text;
+        //                            int first = oldValue.ToLower().IndexOf(myLabel);
+        //                            if (first != -1)
+        //                            {
+        //                                String newValue = oldValue.Substring(0, first != 0 ? first : 0) +
+        //                                                  myValue +
+        //                                                  oldValue.Substring(first + myLabel.Length);
+        //                                if (oldValue != newValue)
+        //                                {
+        //                                    shape.TextFrame.TextRange.Text = newValue;
+        //                                }
+        //                            }
+        //                        }
+        //                    }
+        //                }
+
+        //            }
+        //        }
+        //        oSlides[1].Delete();
+        //        oPowerPoint.Visible = Microsoft.Office.Core.MsoTriState.msoTrue;
+
+        //        //foreach()
+        //        //oShapes = oSlide.Shapes;
+        //        //oShape = oShapes[1];
+        //        //oTxtFrame = oShape.TextFrame;
+        //        //oTxtRange = oTxtFrame.TextRange;
+        //        //oTxtRange.Text = "All-In-One Code Framework";
+
+        //        //// Save the presentation as a pptx file and close it.
+
+        //        //Console.WriteLine("Save and close the presentation");
+
+        //        //string fileName = Path.GetDirectoryName(
+        //        //    Assembly.GetExecutingAssembly().Location) + "\\Sample1.pptx";
+        //        //oPre.SaveAs(fileName,
+        //        //    PowerPoint.PpSaveAsFileType.ppSaveAsOpenXMLPresentation,
+        //        //    Office.MsoTriState.msoTriStateMixed);
+        //        //oPre.Close( );
+
+        //        //// Quit the PowerPoint application.
+
+        //        //Console.WriteLine("Quit the PowerPoint application");
+        //        //oPowerPoint.Quit( );
+        //        this.hasError = false;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        this.error = ex.Message;
+        //    }
+        //    finally
+        //    {
+        //        // Clean up the unmanaged PowerPoint COM resources by explicitly 
+        //        // calling Marshal.FinalReleaseComObject on all accessor objects. 
+        //        // See http://support.microsoft.com/kb/317109.
+
+        //        if (oTxtRange != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oTxtRange);
+        //            oTxtRange = null;
+        //        }
+        //        if (oTxtFrame != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oTxtFrame);
+        //            oTxtFrame = null;
+        //        }
+        //        if (oShape != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oShape);
+        //            oShape = null;
+        //        }
+        //        if (oShapes != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oShapes);
+        //            oShapes = null;
+        //        }
+        //        if (oSlide != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oSlide);
+        //            oSlide = null;
+        //        }
+        //        if (oSlides != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oSlides);
+        //            oSlides = null;
+        //        }
+        //        if (oPre != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oPre);
+        //            oPre = null;
+        //        }
+        //        if (oPres != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oPres);
+        //            oPres = null;
+        //        }
+        //        if (oPowerPoint != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oPowerPoint);
+        //            oPowerPoint = null;
+        //        }
+        //        if (oSheet != null)
+        //        {
+        //            Marshal.FinalReleaseComObject(oSheet);
+        //            oSheet = null;
+        //        }
+        //        if (oWorkBook != null)
+        //        {
+        //            oWorkBook.Close();
+        //            Marshal.FinalReleaseComObject(oWorkBook);
+        //            oWorkBook = null;
+        //        }
+        //    }
+        //}
+
+        private string GetDateFormat()
+        {
+            string format = MySettings.Default.SettingDateFormat;
+            if (String.IsNullOrEmpty(format))
+            {
+                format = DateTimeFormatInfo.CurrentInfo.GetAllDateTimePatterns('d').First();
+                MySettings.Default.SettingDateFormat = format;
+                MySettings.Default.Save();
+            }
+            return format;
+        }
+
+    }
+}
